ca410f7ec949a718cf785e63038fd668ca1879b9
Guilherme Ferreira <guifrribeiro@gmail.com>
2020-04-13 02:12:17 -0300

    Login page, List page and api create

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, AsyncStorage } from 'react-native';
+
+import CourtList from '../components/CourtList';
+
+//import logo from '../assets/logo.png';
 
 export default function List() {
-  return <View />
+  const [sports, setSports] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('sports').then(storagedSports => {
+      const sportsArray = storagedSports.split(',').map(sport => sport.trim());
+
+      setSports(sportsArray);
+    })
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <Image style={styles.logo} source={logo} /> */}
+
+      <CourtList />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, Image, StyleSheet, Text, TextInput, TouchableOpacity, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, KeyboardAvoidingView, Image, StyleSheet, Text, TextInput, TouchableOpacity, Platform } from 'react-native';
+
+import api from '../services/api';
 
 //import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [sports, setSports] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('List');
+      }
+    })
+  }, []);
+
+  async function handleSubmit() {
+    const response = await api.post('/sessions', {
+      email
+    });
+
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+    await AsyncStorage.setItem('sports', sports);
+
+    navigation.navigate('List');
+  }
+
   return (
     <KeyboardAvoidingView enabled={Platform.OS == 'ios'} behavior="padding" style={styles.container}>
-      <Image source={} />
+      {/* <Image source={logo} /> */}
 
       <View style={styles.form}>
         <Text style={styles.label}>SEU E-MAIL *</Text>
@@ -17,6 +43,8 @@ export default function Login() {
           keyboardType="email-address"
           autoCapitalize="none"
           autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
         />
 
         <Text style={styles.label}>ESPORTES *</Text>
@@ -26,9 +54,10 @@ export default function Login() {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
+          onChangeText={setSports}
         />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
           <Text style={styles.buttonText}>Encontrar quadras</Text>
         </TouchableOpacity>
       </View>
@@ -39,7 +68,7 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: center,
+    justifyContent: 'center',
     alignItems: 'center'
   },
   // Styles form
